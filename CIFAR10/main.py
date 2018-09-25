72f999be29eff7a7e983a592e53a66b8491e65d1
Yash Akhauri <akhauri.yash@gmail.com>
2018-09-25 03:10:40 -0400

    MAJOR

---
+++ CIFAR10/main.py
@@ -0,0 +1,175 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import sys
+import os
+import torch
+import argparse
+import torchvision
+import torchvision.transforms as transforms
+import util
+import torch.nn as nn
+import torch.optim as optim
+from models import hbnet
+from torch.autograd import Variable
+
+def save_state(model, optimizer, acc):
+    print("==> Saving model ...")
+    state = {
+            'acc'          : acc,
+            'state_dict' : model.state_dict(),
+            'optimizer'  : optimizer.state_dict(),
+            }
+    torch.save(state, 'models/hbnet.best.pth.tar')
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(trainloader):
+        # Binarization of weights
+        bin_op.binarization()
+        # Forward pass
+        data, target = Variable(data.cuda()), Variable(target.cuda())
+        optimizer.zero_grad()
+        output          = model(data)
+        # Backward pass
+        ## L2-SVM etc.?
+        loss         = criterion(output, target)
+        loss.backward()
+        # Restore full precision weights
+        bin_op.restore()
+        bin_op.updateBinaryGradWeight()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [({:.2f}%)]\tLoss: {:.4f}\tLR: {}'.format(
+                epoch,
+                100*batch_idx / len(trainloader), loss.data[0],
+                optimizer.param_groups[0]['lr']))
+    return
+
+def test():
+    global best_acc
+    model.eval()
+    test_loss = 0
+    correct   = 0
+    bin_op.binarization()
+    for data, target in testloader:
+        data, target = Variable(data.cuda()), Variable(target.cuda())
+        output       = model(data)
+        test_loss   += criterion(output, target).data[0]
+        pred         = output.data.max(1, keepdim=True)[1]
+        correct     += pred.eq(target.data.view_as(pred)).cpu().sum()
+    correct          = float(correct)
+    bin_op.restore()
+    acc              = 100*float(correct)/float(len(testloader.dataset))
+    test_loss        /= float(len(testloader.dataset))
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({}%)'.format(
+        test_loss * 128, correct, len(testloader.dataset),
+        100*float(correct)/float(len(testloader.dataset))))
+    
+    print('Best Accuracy: {}%\n'.format(float(best_acc)))
+
+    if acc > best_acc:
+        best_acc     = acc
+        save_state(model, optimizer, best_acc)
+    return
+
+def adjust_learning_rate(optimizer, epoch):
+    update_list = [120, 200, 240, 280]
+    if epoch in update_list:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = param_group['lr'] * 0.1
+    return
+
+if __name__=='__main__':
+    cpu         =    False
+    data        =    './data'
+    arch        =    'hbnet'
+    lr          =    0.01
+    pretrained  =    False
+    evaluate    =    False
+
+
+    # Seed for reproducibility
+    torch.manual_seed(1)
+    torch.cuda.manual_seed(1)
+
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+
+    trainset    = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+
+    testset     = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader  = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
+
+    # define classes
+    classes     = ('plane', 'car', 'bird', 'cat',
+                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    # define the model
+    print('==> building model ...')
+    if arch    == 'hbnet':
+        model   = hbnet.HbNet()
+    else:
+        raise Exception(arch+' is currently not supported')
+
+    base_lr     = float(lr)
+    param_dict  = dict(model.named_parameters())
+    params      = []
+
+    for key, value in param_dict.items():
+        params += [{'params'      : [value],
+                    'lr'          :    base_lr,
+                    'weight_decay': 0.00001}]
+        optimizer = optim.Adam(params, lr=0.050, weight_decay=0.00001)
+    criterion   = nn.CrossEntropyLoss()
+
+    ## MODEL INITIALIZATION
+    if not pretrained:
+        print('==> Initializing model parameters ...')
+        best_acc = 0
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0, 0.05)
+                m.bias.data.zero_()
+    ## Model loading
+    else:
+        print('==> Load pretrained model form', pretrained, '...')
+        pmod2 = torch.load('models/hbnet.best.pth.tar')
+        pmod  = torch.load('models/test.best.pth.tar')
+        for key, value in pmod2['state_dict'].items():
+            pmod['state_dict'][key[7:]] = value
+        model.load_state_dict(pmod['state_dict'])
+        best_acc = pmod['acc']
+
+    if not cpu:
+        model.cuda()
+        model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+    print(model)
+    if not pretrained:
+        print("Skipping optimizer loading")
+    else:
+           optimizer.load_state_dict(pmod2['optimizer'])
+       
+       if evaluate:
+           test()
+           exit(0)
+
+       for epoch in range(0, 320):
+           adjust_learning_rate(optimizer, epoch)
+           train(epoch)
+           test(optimizer)
---
+++ MNISTMLP/models/hbnet.py
@@ -0,0 +1,253 @@
+import torch
+import os
+import time, math, sys
+import numpy as np
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import torch.nn.functional as F
+from torch.autograd.function import Function, once_differentiable
+from functools import reduce
+from torch.nn.modules.module import _addindent
+from torch.nn._functions.thnn import _all_functions
+from torch.autograd import gradcheck, Variable
+from torch._thnn import type2backend
+from torch.nn.modules.utils import _pair
+
+binAgg = 16
+
+# For proving angle preservation property
+def angle(a, b):
+    cos = nn.CosineSimilarity()
+    return math.degrees(math.acos(cos(a.view(1, -1),b.view(1, -1))))
+
+
+def binAbs(input):
+    shape   =   input.size()
+    restore = 0
+    if(len(shape)==4):
+        restore     =   input.size()
+        input       =   input.reshape(shape[0], -1)
+    shape           =   input.size()
+    if(len(shape)==2): 
+        input       =   input.unsqueeze(1)
+    shape = input.size()
+    if(shape[-1] > binAgg):
+        listmat = list(torch.split(torch.abs(input), binAgg, dim=-1))
+        residualmat = listmat[-1]
+        splitup = torch.stack(listmat[:-1])
+        stackmat = torch.mean(splitup, dim=-1, keepdim=True).repeat(1, 1, 1, listmat[0].size(-1))
+        residualmat = torch.mean(residualmat, dim=-1, keepdim=True).repeat(1, 1, residualmat.size(-1))
+        z = list(stackmat)
+        stackmat = torch.cat(z, dim=-1)
+        output = torch.cat((stackmat, residualmat), dim=-1)
+    else:
+        listmat = list(torch.split(torch.abs(input), binAgg, dim=-1))
+        splitup = torch.stack(listmat)
+        stackmat = torch.mean(splitup, dim=-1, keepdim=False).repeat(1, 1, 1, listmat[0].size(-1))
+        z = list(stackmat)
+        output = torch.cat(z, dim=-1)
+    output = torch.squeeze(output) 
+    if(restore!=0):
+        output = output.reshape(restore)
+    return output
+
+
+
+class BinActive(Function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        shape   =   input.size()
+        if(len(shape) == 2):  
+            input = input.unsqueeze(1)
+        shape   =   input.size()
+        if(shape[-1] > binAgg):
+            binmat  =   input.sign()
+            listmat = list(torch.split(torch.abs(input), binAgg, dim=-1))
+            residualmat = listmat[-1]
+            splitup = torch.stack(listmat[:-1])
+            stackmat = torch.mean(splitup, dim=-1, keepdim=True).repeat(1, 1, 1, listmat[0].size(-1))
+            residualmat = torch.mean(residualmat, dim=-1, keepdim=True).repeat(1, 1, residualmat.size(-1))
+            z = list(stackmat)
+            stackmat = torch.cat(z, dim=-1)
+            output = torch.cat((stackmat, residualmat), dim=-1)
+            output = torch.squeeze(output.mul(binmat))
+        else:
+            binmat = input.sign()
+            listmat = list(torch.split(torch.abs(input), binAgg, dim=-1))
+            splitup = torch.stack(listmat)
+            stackmat = torch.mean(splitup, dim=-1, keepdim=False).repeat(1, 1, 1, listmat[0].size(-1))
+            z = list(stackmat)
+            output = torch.cat(z, dim=-1)
+            output = torch.squeeze(output.mul(binmat))
+        return output
+    @staticmethod
+    def backward(ctx, grad_output):
+        input,             =    ctx.saved_tensors
+        output             =    bingradupd(input)
+        binAgg             =    16
+        grad_input         =    grad_output.clone()
+        g_out              =    grad_output.clone()
+        s                  =    g_out.size()
+        grad_input[input.le(-1.0)] = 0
+        grad_input[input.ge( 1.0)] = 0
+        m = output.abs().mul(grad_input)
+        ## Here, div(g_out.nelement()) should be binAgg by proof. 
+        ## but that does not work.
+        m_add = (g_out.mul(input.sign())).sum().div(g_out.nelement()).expand(s)
+        m = m.add(m_add)
+        return m
+
+
+class Im2Col(Function):
+    @staticmethod
+    def forward(ctx, input, kernel_size, dilation, padding, stride):
+        assert input.dim() == 4
+        ctx.kernel_size = kernel_size
+        ctx.dilation = dilation
+        ctx.padding = padding
+        ctx.stride = stride
+        ctx.input_size = (input.size(2), input.size(3))
+        ctx._backend = type2backend[input.type()]
+        output = input.new()
+        ctx._backend.Im2Col_updateOutput(ctx._backend.library_state,        \
+                                         input, output,                     \
+                                         kernel_size[0], kernel_size[1],    \
+                                         dilation[0], dilation[1],          \
+                                         padding[0], padding[1],            \
+                                         stride[0], stride[1])              
+        return output
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_output):
+        grad_input = grad_output.new()
+        ctx._backend.Im2Col_updateGradInput(ctx._backend.library_state,             \
+                                            grad_output,                            \
+                                            grad_input,                             \
+                                            ctx.input_size[0], ctx.input_size[1],   \
+                                            ctx.kernel_size[0], ctx.kernel_size[1], \
+                                            ctx.dilation[0], ctx.dilation[1],       \
+                                            ctx.padding[0], ctx.padding[1],         \
+                                            ctx.stride[0], ctx.stride[1])           
+        return grad_input, None, None, None, None
+
+class Col2Im(Function):
+    @staticmethod
+    def forward(ctx, input, kernel_size, dilation, padding, stride):
+        img_h = (torch.sqrt(torch.FloatTensor([input.size(-1)])) - 1)*stride[0] - 2*padding[0] + kernel_size[0]
+        output_size = (img_h, img_h)
+        ctx.output_size = output_size
+        ctx.kernel_size = kernel_size
+        ctx.dilation = dilation
+        ctx.padding = padding
+        ctx.stride = stride
+        ctx._backend = type2backend[input.type()]
+        output = input.new()
+        ctx._backend.Col2Im_updateOutput(ctx._backend.library_state,
+                                         input, output,
+                                         int(output_size[0].item()), int(output_size[1].item()),
+                                         kernel_size[0], kernel_size[1],
+                                         dilation[0], dilation[1],
+                                         padding[0], padding[1],
+                                         stride[0], stride[1])
+        return output
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_output):
+        grad_input = grad_output.new()
+        ctx._backend.Col2Im_updateGradInput(ctx._backend.library_state,
+                                            grad_output,
+                                            grad_input,
+                                            ctx.kernel_size[0], ctx.kernel_size[1],
+                                            ctx.dilation[0], ctx.dilation[1],
+                                            ctx.padding[0], ctx.padding[1],
+                                            ctx.stride[0], ctx.stride[1])
+        return grad_input, None, None, None, None, None
+
+
+class hbPass(nn.Module):
+    def __init__(self, input_channels, output_channels,                     \
+            kernel_size=-1, stride=-1, padding=-1, dropout=0, groups=1, Linear=False):
+        super(hbPass, self).__init__()
+        ##########################################################
+        self.layer_type     =   'hbPass'
+        self.kernel_size    =   kernel_size
+        self.stride         =   stride
+        self.padding        =   padding
+        self.Linear         =   Linear
+        self.binagg         =   16
+        ##########################################################
+        self.dropout_ratio  =   dropout
+        if dropout!=0:
+            self.dropout    =   nn.Dropout(dropout)    
+        self.linear         =   nn.Linear(input_channels, output_channels)
+        ##########################################################
+        self.binactive      =   BinActive.apply
+        if not self.Linear:
+            self.FPconv     =   nn.Conv2d(input_channels, output_channels,
+                                kernel_size=kernel_size, stride=stride, padding=padding, groups=groups)
+            self.im2col     =   Im2Col(kernel_size=_pair(kernel_size), dilation=_pair(1), padding=_pair(padding), stride=_pair(stride))
+            self.col2im     =   Col2Im(kernel_size=_pair(kernel_size), dilation=_pair(1), padding=_pair(padding), stride=_pair(stride))
+            self.bn         =   nn.BatchNorm2d(input_channels, eps=1e-4, momentum=0.1, affine=True)
+        ##########################################################
+        else:
+            self.bn         =   nn.BatchNorm1d(input_channels, eps=1e-4, momentum=0.1, affine=True)
+        ##########################################################
+        self.relu = nn.ReLU(inplace = True)
+        ##########################################################
+    def forward(self, x, kernel_size=_pair(3), dilation=_pair(1), padding=_pair(0), stride=_pair(1)):
+        if not self.Linear:
+            x = self.bn(x)
+            ##########################################################
+            x               =   self.im2col.apply(x, _pair(kernel_size), _pair(dilation), _pair(padding), _pair(stride))
+            x               =   self.binactive(x)
+            x               =   self.col2im.apply(x, _pair(kernel_size), _pair(dilation), _pair(padding), _pair(stride))
+            if self.dropout_ratio!=0:
+                x           =   self.dropout(x)
+            x               =   self.FPconv(x)
+            ##########################################################
+        else:
+            ##########################################################
+            x               =   self.binactive(x)
+            x               =   self.linear(x)
+            ##########################################################
+        x = self.relu(x)
+        return x
+
+
+'''
+	For hbPass
+		if Convolutional
+			BN --> IM2COL --> BINACTIVE --> COL2IM --> DROPOUT --> CONVOLUTION --> RELU
+		if Linear
+			BINACTIVE --> LINEAR --> RELU
+'''
+
+
+class HbNet(nn.Module):
+    def __init__(self):
+        super(HbNet, self).__init__()
+        self.xnor = nn.Sequential(
+                nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+                nn.ReLU(inplace=True),
+                hbPass(192, 160, kernel_size=1, stride=1, padding=0),
+                hbPass(160,  96, kernel_size=1, stride=1, padding=0),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+                hbPass( 96, 192, kernel_size=5, stride=1, padding=2, dropout=0.5),
+                hbPass(192, 192, kernel_size=1, stride=1, padding=0),
+                hbPass(192, 192, kernel_size=1, stride=1, padding=0),
+                nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+
+                hbPass(192, 192, kernel_size=3, stride=1, padding=1, dropout=0.5),
+                hbPass(192, 192, kernel_size=1, stride=1, padding=0),
+                nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+                nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
+                nn.ReLU(inplace=True),
+                nn.AvgPool2d(kernel_size=8, stride=1, padding=0)
+                )
+    def forward(self, x):
+        x = self.xnor(x)
+        x = x.view(x.size(0), 10)
+        return x
